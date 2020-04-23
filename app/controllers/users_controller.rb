0ca167616a725d65c49e7e3a8f0ed95f4abdaa93
名前 <>
2020-04-23 23:07:30 +0900

    [Update]5-9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def update
     @user=User.find(params[:id])#更新は、そのidに上書き処理
     if @user.update(user_params)
-       redirect_to user_path(@user.id),notice: "You have updated user successfully."
+       redirect_to user_path(@user),notice: "You have updated user successfully."
     else
        render action: :edit
     end
@@ -26,6 +26,6 @@ class UsersController < ApplicationController
 
   private
 	def user_params
-		params.require(:user).permit(:name,:profile_image_id,:introduction,)
+		params.require(:user).permit(:name,:profile_image,:introduction,)
 	end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e36d2d47844993f745b52b062b19aff1dfdec8ae96842f3fa319d83ed78a055ea29a696d034e3460d74f2c944380f95f4e391c7c4110dfbd2b5bfa651de698aa'
